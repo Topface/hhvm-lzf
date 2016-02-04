31d2fc0cde502a275385775a90eaf5389ca7bb10
Dmitry Faleychik <fadimko@gmail.com>
2016-02-04 16:08:56 +0300

    removed tests/ directory

--- tests/002.php
+++
@@ -1,5 +0,0 @@
-<?php
-$compressed = lzf_compress("This is test of LZF extension");
-
-echo base64_encode($compressed);
-?>
--- tests/003.php
+++
@@ -1,5 +0,0 @@
-<?php
-$decompressed = lzf_decompress(base64_decode("BFRoaXMgIAIUdGVzdCBvZiBMWkYgZXh0ZW5zaW9u"));
-
-echo $decompressed;
-?>
